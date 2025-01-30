87076037e583b378efab2462b80ef13c5c25f017
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-30 11:11:51 +0700

    Initialize GUI application with basic structure and configurations

---
+++ src/main/java/com/coffeecode/App.java
@@ -0,0 +1,15 @@
+package com.coffeecode;
+
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public App() {
+        setTitle("Hello World");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
