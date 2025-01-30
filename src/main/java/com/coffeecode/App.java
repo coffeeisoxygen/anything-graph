69e955b6940c38a79cf8b8ab951de486b5186d7f
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-30 14:30:32 +0700

    Add PointData, PointNode, and PointEdge classes with validation and graph functionality

--- src/main/java/com/coffeecode/App.java
+++ src/main/java/com/coffeecode/App.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("the journey just begin");
-        log.info("the journey just begin");
+        log.info("Starting application...");
+
     }
 }
